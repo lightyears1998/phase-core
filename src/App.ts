35a16a6dc90a84fc0afc0af1c7320d122697d219
lightyears <lightyears1998@hotmail.com>
2019-11-04 19:05:07 +0800

    修正文件名大小写b

---
+++ src/App.ts
@@ -0,0 +1,52 @@
+import os from "os";
+import fs from "fs";
+import path from "path";
+import figlet from "figlet";
+
+
+export class App {
+  configPath: string
+  dataPath: string
+
+  constructor() {
+    this.setPath();
+    this.ensurePath();
+  }
+
+  private setPath(): void {
+    const platform = os.platform();
+    const userHome = os.homedir();
+
+    switch (platform) {
+        case "win32": {
+          const projectHome = path.resolve(userHome + "/AppData/Local/Phase");
+          this.configPath = path.resolve(projectHome + "/config.json");
+          this.dataPath = path.resolve(projectHome + "/data.sqlite3")
+          break;
+        }
+
+        // case "linux": {
+        //   this.configPath = path.resolve();
+        //   this.dataPath = path.resolve()
+        //   break;
+        // }
+
+        default: {
+          throw "不支持的平台：" + platform;
+        }
+    }
+  }
+
+  private ensurePath() : void {
+    
+  }
+
+  public start(): void {
+    this.printTitle();
+  }
+
+  private printTitle(): void {
+    const textArt = (text): string => figlet.textSync(text, "Star Wars");
+    console.log(textArt("Phase"));
+  }
+}
