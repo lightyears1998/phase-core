7f10530916a59391bba0897a59c3da80000528b9
lightyears <lightyears1998@hotmail.com>
2019-12-13 16:57:52 +0800

    细化Time

--- src/entity/Time.ts
+++ src/entity/Time.ts
@@ -2,6 +2,27 @@ import { Column } from "typeorm";
 
 export class Time {
     @Column() start: Date
-    @Column() end: Date
     @Column() span: Date
+
+    public constructor(start?: Date, span?: Date) {
+        this.start = start;
+        this.span = span;
+    }
+
+    public setTimeStartAndSpan(start: Date, span: Date): void {
+        this.start = start;
+        this.span = span;
+    }
+
+    public setTimeStartAndEnd(start: Date, end: Date): void {
+        this.start = start;
+        this.span = new Date(end.getTime() - start.getTime());
+    }
+
+    public get end(): Date | null {
+        if (this.start && this.span) {
+            return new Date(this.start.getTime() + this.span.getTime());
+        }
+        return null;
+    }
 }
---
+++ src/view/SetupMenu.ts
@@ -0,0 +1,7 @@
+// import { DistinctQuestion } from "inquirer";
+
+/*
+ * 打印当前配置文件
+ * 询问是否要更改配置文件
+ * 询问是否更新Hitokoto
+ */
