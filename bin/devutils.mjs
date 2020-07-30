1d587ea3e217da8b2c776f2059aaf1ce14c3e268
lightyears1998 <lightyears1998@xiyanghui.com>
2020-07-30 13:38:14 +0800

    使用激进的运行策略

---
+++ bin/devutils.mjs
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { remove } from "fs-extra";
+import { argv } from "yargs";
+import { resolve } from "path";
+
+const projectRoot = resolve(__dirname, "../");
+const option = argv._.shift();
+
+
+if (option === "clean") {
+    remove(resolve(projectRoot, "./lib"));
+    remove(resolve(projectRoot, "./var"));
+    remove(resolve(projectRoot, "./ormlogs.log"));
+    remove(resolve(projectRoot, "./yarn-error.log"));
+} else {
+    console.log("Do nothing.");
+}
