6532a2b76f3b8d7d223f63e00c138fcd4564d7e8
lightyears1998 <lightyears1998@hotmail.com>
2020-08-07 15:04:15 +0800

    添加关于页面

---
+++ src/view/AboutView.ts
@@ -0,0 +1,16 @@
+import { View } from "./common";
+import marked, { Renderer as MarkedRender } from "marked";
+import MarkedTerminalRender from "marked-terminal";
+
+
+export class AboutView extends View {
+    public introduction = `# Day Primer
+
+Goal-driven action assistance software.
+
+\`code\` with ♥ by *Day Primer* developers.`
+
+    public async invoke(): Promise<void> {
+        console.log(marked(this.introduction, { renderer: new MarkedTerminalRender() }));
+    }
+}
--- src/view/SetupView.ts
+++ src/view/SetupView.ts
@@ -1,8 +1,12 @@
-import { ListQuestion } from "inquirer";
 import { RouterView, Route } from "./common";
 import { UserView } from "./UserView";
+import { AboutView } from "./AboutView";
 
 
 export class SetupView extends RouterView {
-    protected choices = [new Route("用户管理", new UserView()), new Route("返回主菜单", null)]
+    protected choices = [
+        new Route("用户管理", new UserView()),
+        new Route("返回主菜单", null),
+        new Route("关于", new AboutView())
+    ]
 }
